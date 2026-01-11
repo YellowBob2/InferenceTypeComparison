f6216dc0e2af86e14615a05d8818953cf9019392
BigBoyPro <BigBoyPro@proton.me>
2026-01-12 00:39:24 +0100

    demo 1 fin

--- DEMOS/TYPAGE NOMINATIF/haskell.hs
+++ DEMOS/TYPAGE NOMINATIF/haskell.hs
@@ -3,18 +3,17 @@
 -- === 1. Inférence de type de base ===
 -- Aucune annotation de type requise, Haskell déduit les types
 
-
-add x y = x + y  -- infere: add :: Num a => a -> a -> a
+add x y = x + y  -- infére:     add :: Num a => a -> a -> a
 
 example1 :: IO ()
 example1 = do
     putStrLn "\n=== 1. TYPE INFERENCE (Hindley-Milner) ==="
     print (add 1 2)           -- 3
     print (add 1.2 3.4)       -- 4.6
-    print (add 5 0.5)           -- 5.5 -- 5 inferee comme Double pour eviter erreur
+    print (add 5 0.5)         -- 5.5   -   5 inférée comme Double pour éviter erreur
     let a = 5
     let b = 5.5
-    print (add a b)           -- 10.5
+    print (add a b)           -- 10.5  -   a inférée comme Double pour éviter erreur
 
     -- types mixtes ne sont pas autorisés
     let c :: Int
---
+++ DEMOS/TYPAGE NOMINATIF/ocaml.ml
@@ -0,0 +1,34 @@
+(* === OCaml === *)
+
+
+(* === 1. Inférence de type de base === *)
+(* Pas de Surcharge, pas de classes de types *)
+let add x y = x + y
+let add_float x y = x +. y
+let a = add 5 3 
+let b = add_float 5.5 3.5 
+
+
+
+
+(* OCaml a des limites aussi dans son inférence globale *)
+
+type eur = { amount : float }
+
+(* fonction de conversion de euros en cents *)
+let to_cents_eur e = e.amount *. 100.0 (* infére: eur -> float *)
+
+(* Je définis un autre type usd avec le même champ 'amount' *)
+type usd = { amount : float }
+
+let mon_portefeuille = { amount = 50.0 } (* infére: usd car c'est la dernière déf. *)
+
+let v = to_cents_eur mon_portefeuille (* ERREUR DE COMPILATION IMMÉDIATE *)
+(* L'inférence dit : "J'attendais un type 'eur', j'ai reçu un 'usd'. C'est NON." *)
+
+let v = to_cents_eur { amount = 50.0 } (* Ça marche ! *)
+
+(* Main *)
+let () = Printf.printf "Valeur en cents: %.2f\n" v
+  
+
